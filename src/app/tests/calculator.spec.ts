ae5f477e37103d8159c5f7369261d35701fb5a3d
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-10-25 09:24:42 -0500

    Configuracion de reportes con Mocha.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/tests/calculator.spec.ts
+++ src/app/tests/calculator.spec.ts
@@ -3,14 +3,26 @@ import { Calculator } from "./calculator";
 describe("Test for Calculator class", () => {
   const calculator = new Calculator();
 
-  it("Answere should be 49", () => {
-    const ans = calculator.multiply(7, 7);
-    expect(ans).toEqual(49);
+  describe("Multiplication", () => {
+    it("Answere should be 49", () => {
+      const ans = calculator.multiply(7, 7);
+      expect(ans).toEqual(49);
+    });
+    it("Answere should be 0", () => {
+      const ans = calculator.multiply(7, 0);
+      expect(ans).toEqual(0);
+    });
   });
 
-  it("Answere should be Infinity", () => {
-    const ans = calculator.divide(7, 0);
-    expect(ans).toEqual(Infinity);
+  describe("Division", () => {
+    it("Answere should be Infinity", () => {
+      const ans = calculator.divide(7, 0);
+      expect(ans).toEqual(Infinity);
+    });
+    it("Answere should be 2", () => {
+      const ans = calculator.divide(48, 24);
+      expect(ans).toEqual(2);
+    });
   });
 
   it("Test matchers", () => {
